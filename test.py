214790b94b1512fef80fe274c88c4b42b6f2b45f
Sreeraj-S <56246216+Sreeraj-S@users.noreply.github.com>
2019-10-13 18:28:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def wishMe():
     else:
         speak("Good Evening!")  
 
-    speak("I am Jarvis Sir. Please tell me how may I help you")       
+    speak("I am Viki Sir. Please tell me how may I help you")       
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
@@ -49,6 +49,22 @@ def takeCommand():
         print("Say that again please...")  
         return "None"
     return query
+def WakeUp():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        #print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:    
+        query = r.recognize_google(audio, language='en-in')
+        #print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+          
+        return #"None"
+    return query
 
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -61,49 +77,97 @@ def sendEmail(to, content):
 if __name__ == "__main__":
     wishMe()
     while True:
-        if 
-        query = takeCommand().lower()
+        Wish = WakeUp()
+        if Wish=='wiki':
+            query = takeCommand().lower()
+            if 'wikipedia' in query:
+                speak('Searching Wikipedia...')
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+
+            elif 'open youtube' in query:
+                webbrowser.open("youtube.com")
+
+            elif 'open google' in query:
+                webbrowser.open("google.com")
+
+            elif 'open stackoverflow' in query:
+                webbrowser.open("stackoverflow.com")   
+
+
+            elif 'play music' in query:
+                music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+                songs = os.listdir(music_dir)
+                print(songs)    
+                os.startfile(os.path.join(music_dir, songs[0]))
+
+            elif 'the time' in query:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+                speak(f"Sir, the time is {strTime}")
+
+            elif 'open code' in query:
+                codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+                os.startfile(codePath)
+
+            elif 'email to harry' in query:
+                try:
+                    speak("What should I say?")
+                    content = takeCommand()
+                    to = "yourEmail@gmail.com"    
+                    sendEmail(to, content)
+                    speak("Email has been sent!")
+                except Exception as e:
+                    print(e)
+                    speak("Sorry my friend harry bhai. I am not able to send this email") 
+            elis 'shutdown wiki' in query:
+                speak('are you sure')
+                content=takecommand()
+                if 'yes'==content or 'sure' == content:
+                    speak('bye')
+                    quit()
+                elif 'no'==content or 'nah'==content:
+                    pass
 
         
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
-        elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
-
-        elif 'open google' in query:
-            webbrowser.open("google.com")
-
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
-
-
-        elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
-
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-
-        elif 'email to harry' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "yourEmail@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")   
+        
+        
